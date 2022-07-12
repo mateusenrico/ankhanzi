c63a71d9ca1b5575f7959844f9104ab23e4bd630
Mateus Enrico Caruso <mateusenrico.mec@gmail.com>
2022-07-12 20:32:55 -0300

    Dict handler

--- src/main.rs
+++ src/main.rs
@@ -3,16 +3,7 @@ use crate::lib::Dict;
 
 // TODO: acrescentar documentação
 fn main() {
-    let dict = Dict::new();
+    let dict = Dict::init();
 
-    let unicos = dict
-        .list
-        .values()
-        .collect::<Vec<_>>()
-        .into_iter()
-        .filter(|x| x.tags.len() == 0)
-        .collect::<Vec<_>>();
-
-    println!("TAMANHO UNICO: {}", unicos.len());
     dict.export();
 }
